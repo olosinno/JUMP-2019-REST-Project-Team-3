7cae9dcfd443e9b704c9be8f085837d983c95d18
Gregory <itsgregreed@gmail.com>
2019-09-25 10:08:23 -0400

    MYSQL Done

--- AnimalShelterREST/src/main/java/com/collabera/three/animalshelter/controller/AnimalShelterController.java
+++ AnimalShelterREST/src/main/java/com/collabera/three/animalshelter/controller/AnimalShelterController.java
@@ -1,8 +1,38 @@
 package com.collabera.three.animalshelter.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
-public class AnimalShelterController {
+import com.collabera.three.animalshelter.model.Foster;
+import com.collabera.three.animalshelter.repository.FosterRepository;
 
+@RestController
+@RequestMapping(path = "/animalshelter")
+public class AnimalShelterController
+{
+	@Autowired
+	
+	private FosterRepository fosterRepository;
+	
+	@PostMapping(path = "/addFoster")
+	public @ResponseBody String addNewFoster (@RequestParam String name, @RequestParam String animalType)
+	{
+		Foster tmpFost = new Foster();
+		tmpFost.setAnimalName(name);
+		tmpFost.setAnimalType(animalType);
+		fosterRepository.save(tmpFost);
+		return "Saved";
+	}
+	
+	@GetMapping(path = "/allFoster")
+	public @ResponseBody Iterable<Foster> getAlFosters()
+	{
+		return fosterRepository.findAll();
+	}
 }
